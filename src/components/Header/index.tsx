fe3bcb3f96ac3686dbc4ea4affc94e2a0918d87f
Thiago Miziara <thiago.miziara@luizalabs.com>
2022-09-23 15:03:35 -0300

    definindo header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,21 @@
+import { HeaderContainer } from './styles'
+import { Timer, Scroll } from 'phosphor-react'
+import logoImg from '../../assets/logo-ignite.svg'
+import { NavLink } from 'react-router-dom'
+
 export function Header() {
-  return <h1>Header</h1>
+  return (
+    <HeaderContainer>
+      <img src={logoImg} alt="" />
+
+      <nav>
+        <NavLink to="/" end title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
 }
