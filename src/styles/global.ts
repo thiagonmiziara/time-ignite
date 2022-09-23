54f1de9c5a5311fd062c311cff18cb04d8c5083d
Thiago Miziara <thiago.miziara@luizalabs.com>
2022-09-23 15:29:11 -0300

    estilizando a home

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -16,6 +16,7 @@ export const GlobalStyle = createGlobalStyle`
 body{
   background-color: ${({ theme }) => theme['gray-900']};
   color:${({ theme }) => theme['gray-300']};
+  -webkit-font-smoothing: antialiased;
 }
 
 border, input, textarea,button{
