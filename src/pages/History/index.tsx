c4575ef37bfe681b8c94d268b1e791951a7723ff
Thiago Miziara <thiago.miziara@luizalabs.com>
2022-10-04 19:49:22 -0300

    definindo estilos basicos da pagina history

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,61 @@
+import {
+  HistoryContainer,
+  HistoryList,
+  HistoryTable,
+  HistoryTD,
+  HistoryTH,
+} from './styles'
+
 export function History() {
-  return <h1>History</h1>
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <HistoryTable>
+          <thead>
+            <tr>
+              <HistoryTH>Tarefa</HistoryTH>
+              <HistoryTH>Duração</HistoryTH>
+              <HistoryTH>Início</HistoryTH>
+              <HistoryTH>Status</HistoryTH>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <HistoryTD>Tarefa</HistoryTD>
+              <HistoryTD>20 minutos</HistoryTD>
+              <HistoryTD>Há 2 meses</HistoryTD>
+              <HistoryTD>Concluído</HistoryTD>
+            </tr>
+            <tr>
+              <HistoryTD>Tarefa</HistoryTD>
+              <HistoryTD>20 minutos</HistoryTD>
+              <HistoryTD>Há 2 meses</HistoryTD>
+              <HistoryTD>Concluído</HistoryTD>
+            </tr>
+            <tr>
+              <HistoryTD>Tarefa</HistoryTD>
+              <HistoryTD>20 minutos</HistoryTD>
+              <HistoryTD>Há 2 meses</HistoryTD>
+              <HistoryTD>Concluído</HistoryTD>
+            </tr>
+            <tr>
+              <HistoryTD>Tarefa</HistoryTD>
+              <HistoryTD>20 minutos</HistoryTD>
+              <HistoryTD>Há 2 meses</HistoryTD>
+              <HistoryTD>Concluído</HistoryTD>
+            </tr>
+            <tr>
+              <HistoryTD>Tarefa</HistoryTD>
+              <HistoryTD>20 minutos</HistoryTD>
+              <HistoryTD>Há 2 meses</HistoryTD>
+              <HistoryTD>Concluído</HistoryTD>
+            </tr>
+          </tbody>
+        </HistoryTable>
+      </HistoryList>
+    </HistoryContainer>
+  )
 }
